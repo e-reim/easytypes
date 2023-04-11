634587fb7ac7438ef5b09ecd1d98fdd298447b58
Reim, Elijah <Elijah.Reim@wartsila.com>
2023-04-11 12:21:54 +0200

    Some early version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# easytypes change log
+
+## 0.0.1
+
+Initial beta version
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='easytypes',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/e-reim/easytypes',
+    license='Apache Software License',
+    author='Reim, Elijah',
+    author_email='ellioh.72@gmail.com',
+    description='Easily typed structures for Python with optional field presence checking and runtime type checking'
+)
